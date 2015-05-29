b51c4fd00c47983aa2dea3029a57b291f94b357b
31415us <pius.vondaeniken@epfl.ch>
2015-05-29 22:23:18 +0200

    add plot to show simulated measurement

--- measurement_visualizer.py
+++ measurement_visualizer.py
@@ -1,6 +1,7 @@
 import pygame, sys
 
-from math import pi
+from math import pi, sqrt
+from matplotlib import pyplot as plt
 
 from linda.Vec2D import Vec2D
 from linda.Circle import Circle
@@ -33,6 +34,8 @@ WALLS = [BOTTOM_WALL, RIGHT_WALL, TOP_WALL, LEFT_WALL]
 SPEED = 0.1
 OMEGA = pi
 
+PLOT_UPDATE = 3.0
+
 def main():
 
     robot_state = RobotState(1.0, 1.0, 0.0)
@@ -41,10 +44,17 @@ def main():
     backward = False
     left = False
     right = False
+    noisy = False
 
     clk = pygame.time.Clock()
 
     circles = []
+
+    sim = LidarSimulator(default_dist=3.0, nb_samples=100, angular_cutoff=pi)
+    
+    plot_info = init_plot()
+
+    time_acc = 0.0
 
     while True:
         for event in pygame.event.get():
@@ -60,6 +70,8 @@ def main():
                     left = True
                 elif event.key == pygame.K_d:
                     right = True
+                elif event.key == pygame.K_n:
+                    noisy = not noisy
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_w:
                     forward = False
@@ -83,6 +95,19 @@ def main():
         if right:
             robot_state = rotate_robot(robot_state, -OMEGA * delta_t)
 
+        if noisy:
+            noise_sigma = 0.06
+        else:
+            noise_sigma = None
+
+        sim_meas = sim.lidar_sample(robot_state, WALLS + circles, noise_sigma=noise_sigma)
+
+        time_acc += delta_t
+
+        if time_acc > PLOT_UPDATE:
+            time_acc = 0.0
+            plot_measurement(plot_info, sim_meas)
+
         draw_env(WALLS, circles)
 
         draw_robot(robot_state)
@@ -102,6 +127,22 @@ def rotate_robot(robot_state, amnt):
 
 def world_to_screen(p):
     return (int(p.pos_x * PX_PER_METER), HEIGHT - int(p.pos_y * PX_PER_METER))
+
+def init_plot():
+    plt.ion()
+    ax = plt.axes()
+    line, = plt.plot([], 'ro')
+    plt.xlim([-pi, pi])
+    plt.ylim([0.0, 3.0])
+
+    return ax, line
+
+def plot_measurement(plot_info, meas):
+    a, l = plot_info
+    x_vals, y_vals = meas
+    l.set_xdata(x_vals)
+    l.set_ydata(y_vals)
+    plt.pause(0.01)
 
 def draw_env(walls, circles):
     for wall in walls:
